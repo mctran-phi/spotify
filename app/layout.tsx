c920bf4c34c37e3e894abfce7b02e84232754fdb
Minh Tran <pctran98@gmail.com>
2023-09-27 21:51:26 -0700

    Integrated SidebarItem

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Figtree } from 'next/font/google';
+import Sidebar from '@/components/Sidebar';
 
 const font = Figtree({ subsets: ['latin'] })
 
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -36,7 +39,20 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         w-[300px]
         p-2">
         <Box>
-          Sidebar Navigation
+          <div className="
+          flex
+          flex-col
+          gap-y-4
+          px-5
+          py-4
+          ">
+            {routes.map((item) => (
+              <SidebarItem
+                key={item.label}
+                {...item}
+              />
+            ))}
+          </div>
         </Box>
         <Box className="overflow-y-auto h-full">
           Song Library
