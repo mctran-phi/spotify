afec681c1c37c40768584938af859fbe9133633f
Minh Tran <pctran98@gmail.com>
2023-10-11 19:59:25 -0700

    Integrated functionality to render songs from database

---
+++ components/SongItem.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import useLoadImage from "@/hooks/useLoadImage";
+import Image from "next/image";
+import { Song } from "@/types";
+
+interface SongItemProps {
+  data: Song;
+  onClick: (id: string) => void
+}
+
+const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
+  const imagePath = useLoadImage(data);
+
+  return (
+    <div
+      onClick={() => onClick(data.id)}
+      className="
+        relative
+        group
+        flex
+        flex-col
+        items-center
+        justify-center
+        rounded-md
+        overflow:hidden
+        gap-x-4
+        bg-neutral-400/5
+        cursor:pointer
+        hover:bg-neutral-400/10
+        transition
+        p-3
+      ">
+      <div className="
+        relative
+        aspect-square
+        w-full
+        h-full
+        rounded-md
+        overflow-hidden
+      ">
+        <Image
+          className="object-cover"
+          src={imagePath || '/images/liked.png'}
+          fill
+          alt="Image"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SongItem;
