63dd9960e1b3fc249566a7993c122743676663e6
Minh Tran <pctran98@gmail.com>
2023-10-13 00:04:18 -0700

    Rendering users songs on sidebar

--- components/Library.tsx
+++ components/Library.tsx
@@ -5,8 +5,14 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -60,7 +66,13 @@ const Library = () => {
         mt-4
         px-3
       ">
-        Song List
+        {songs.map((item) => (
+          <MediaItem
+            onClick={() => {}}
+            key={item.id}
+            data={item}
+          />
+        ))}
       </div>
     </div>
   );
