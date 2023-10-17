f9bf122e2c15accb9aef2f1656fcaf847223bc15
Minh Tran <pctran98@gmail.com>
2023-10-16 23:05:50 -0700

    Implemented search functionality to search for specific songs

---
+++ components/SearchInput.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import useDebounce from "@/hooks/useDebounce";
+import qs from "query-string";
+import Input from "./Input";
+
+const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debounceValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = {
+      title: debounceValue
+    };
+
+    const url = qs.stringifyUrl({
+      url: '/search',
+      query: query
+    });
+
+    router.push(url);
+  }, [debounceValue, router]);
+
+  return (
+    <Input
+      placeholder="Search Song"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+}
+
+export default SearchInput;
