9086ab11fbc2773d2c59fef741f4d25b6169c976
Minh Tran <pctran98@gmail.com>
2023-10-23 22:02:58 -0700

    Integrated functionality to play songs and configure volume

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -7,6 +7,9 @@ import MediaItem from "./MediaItem";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import { HiSpeakerXMark, HiSpeakerWave } from "react-icons/hi2";
 import Slider from "./Slider";
+import usePlayer from "@/hooks/usePlayer";
+import useSound from "use-sound";
+import { useState, useEffect } from "react";
 
 interface PlayerContentProps {
   song: Song;
@@ -14,8 +17,80 @@ interface PlayerContentProps {
 }
 
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
-  const Icon = true ? BsPauseFill : BsPlayFill;
-  const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+  const player = usePlayer();
+  const [volume, setVolume] = useState(1);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+  const onPlayNext = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const nextSong = player.ids[currentIndex + 1];
+
+    if (!nextSong) {
+      return player.setId(player.ids[0]);
+    }
+
+    player.setId(nextSong);
+  };
+
+  const onPlayPrev = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const prevSong = player.ids[currentIndex - 1];
+
+    if (!prevSong) {
+      return player.setId(player.ids[player.ids.length - 1]);
+    }
+
+    player.setId(prevSong);
+  };
+
+  const [play, { pause, sound }] = useSound(
+    songUrl,
+    {
+      volume: volume,
+      onplay: () => setIsPlaying(true),
+      onend: () => {
+        setIsPlaying(false);
+        onPlayNext();
+      },
+      onpause: () => setIsPlaying(false),
+      format: ['mp3']
+    }
+  );
+
+  useEffect(() => {
+    sound?.play();
+
+    return () => {
+      sound?.unload();
+    }
+  }, [sound]);
+
+  const handlePlay = () => {
+    if (!isPlaying) {
+      play();
+    } else {
+      pause();
+    }
+  };
+
+  const toggleMute = () => {
+    if (volume === 0) {
+      setVolume(1);
+    } else {
+      setVolume(0);
+    }
+  };
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
       <div className="
@@ -37,7 +112,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
         items-center
       ">
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="
             h-10
             w-10
@@ -64,7 +139,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
         gap-x-6
       ">
         <AiFillStepBackward
-          onClick={() => {}}
+          onClick={onPlayPrev}
           size={30}
           className="
             text-neutral-400
@@ -74,7 +149,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
           "
         />
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="
             flex
             items-center
@@ -89,7 +164,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
             <Icon className="text-black" size={30} />
         </div>
         <AiFillStepForward
-          onClick={() => {}}
+          onClick={onPlayNext}
           size={30}
           className="
             text-neutral-400
@@ -102,11 +177,14 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
       <div className="hidden md:flex w-full justify-end pr-2">
         <div className="flex items-center gap-x-2 w-[120px]">
           <VolumeIcon
-            onClick={() => {}}
+            onClick={toggleMute}
             size={34}
             className="cursor-pointer"
           />
-          <Slider />
+          <Slider
+            value={volume}
+            onChange={(value) => setVolume(value)}
+          />
         </div>
       </div>
     </div>
